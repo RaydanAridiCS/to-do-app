8f2923372f91153831a204ca49bc155d7d2fcfb5
RaydanAridiCS <RaydanAridi.CS@gmail.com>
2025-04-15 17:20:52 +0300

    Improved the pop-up style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     const addTaskBtn = document.getElementById("new-task-btn");
     const taskFormContainer = document.getElementById("task-form-container");
@@ -6,15 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const cancelTaskBtn = document.getElementById("cancel-task-btn");
 
     addTaskBtn.addEventListener("click", function() {
-        taskFormContainer.style.display = "block";
-        addTaskBtn.style.display = "none";
-        if (taskForm) {
-            taskForm.reset();
-        }
+      taskFormContainer.classList.remove('hidden');
+      taskFormContainer.classList.add('visible');
+      addTaskBtn.style.display = "none";
+      if (taskForm) {
+        taskForm.reset();
+      }
     });
 
     cancelTaskBtn.addEventListener("click", function() {
-        taskFormContainer.style.display = "none";
-        addTaskBtn.style.display = "block";
+      taskFormContainer.classList.remove('visible');
+      taskFormContainer.classList.add('hidden');
+      addTaskBtn.style.display = "block";
     });
-});
+  });
